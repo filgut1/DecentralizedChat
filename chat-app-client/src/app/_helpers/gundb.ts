77bea03574b49ce95df478bf9b8318ad170f7c06
Filip <fgutica@gmail.com>
2021-03-07 21:55:38 -0800

    Remove unecesarry code

--- chat-app-client/src/app/_helpers/gundb.ts
+++ chat-app-client/src/app/_helpers/gundb.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { User } from '@app/_models';
 
 import Gun from 'gun/gun';
